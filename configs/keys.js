10f8e95bee0f30431600bd0fdfb2427794c8f260
Ricardo-1998 <00004817@uca.edu.sv>
2019-11-10 19:02:59 -0600

    arreglando key de bd

--- configs/keys.js
+++ configs/keys.js
@@ -1,9 +1,5 @@
 module.exports = {
-    mongodb:
-    {
-        //URI: 'mongodb://localhost:27017/GOTH1'
-        URI: 'mongodb://peche:peche123@ds125489.mlab.com:25489/gothmusic'
-
-        //URI: 'mongodb://peche:colademax@ds241258.mlab.com:41258/goth1'
+    mongodb:{
+        URI: 'mongodb://peche:peche123@ds233288.mlab.com:33288/goth'
     }
 }   
