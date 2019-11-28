058ed62924034a3d7408ad080ccf0423a1e38ca4
Ricardo-1998 <00004817@uca.edu.sv>
2019-11-28 00:02:27 -0600

    haciendo merge manual

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+let cant=0;
+
 function guardar() {
     let data =document.getElementById("nombre").value;
     let tbody = document.getElementsByClassName("posts")[0];
@@ -103,7 +105,7 @@ let app = {
                     }
                     div = `<div class="card" style = "background-color: #40445a">
                                         <div class="card-content cancion3" >
-                                            <img src = "images/${data.img}" style = "width: 100%">
+                                            <img src = "images/${data.img}" style = "width: 100%" onClick="cambioEstado()">
                                             <h5>${data.tit}</h5>
                                             <p>${data.usr}</p>
                                             <table>
@@ -265,58 +267,103 @@ function mostrar0(cant1,cant2,estado) {
         })
 }
 
-function alerta() {
+function filtrado() {
     let data1 = document.getElementById("filtro").value;
-    let contP = 0;
-    fetch('/api/post', {
+    if(data1!="all"){
+        let contP = 0;
+        fetch('/api/post', {
+            method: "GET"
+        }).then(res => res.json())
+            .then(response => {
+                //var respo = [];   
+                // respo.add(response[i])
+                let parent = document.getElementById("sec1");
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                };
+                parent.innerHTML = `<ul class="pagination" style="position: absolute; left: 25%; bottom: 0;">                           
+                                        <li class="waves-effect"><a href="#!" onClick="mostrar0(0,3,1)">1</a></li>
+                                        <li class="waves-effect"><a href="#!" onClick="mostrar0(3,6,2)">2</a></li>
+                                        <li class="waves-effect"><a href="#!" onClick="mostrar0(6,9,3)">3</a></li>
+                                        <li class="waves-effect"><a href="#!" onClick="mostrar0(9,12,4)">4</a></li>
+                                        <li class="waves-effect"><a href="#!" onClick="mostrar0(12,15,5)">5</a></li>
+                                    </ul>`;
+                let cont = 1;
+                let aux=0;
+                let cantP=0;
+                do{
+                    let idd = 'modal';
+                    let hash = '#';
+                    console.log(response.posts);
+                    if(data1==(response.posts[aux]).titulo){
+                        let data = {
+                            tit: (response.posts[aux]).titulo,
+                            cuer: (response.posts[aux]).texto,
+                            aut: (response.posts[aux]).autor
+                        };
+                        let tbody = document.getElementById("sec1");
+                        let div = document.createElement("div");
+                        div.innerHTML = `<div class="sectionn">
+                                            <div class="">
+                                            <div class="card-panel" style = "background-color: #40445a">
+                                            <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
+                                                ${data.tit}</a>
+                                            <a class="linkss waves effect waves-light">${data.aut}</a>
+                                            <p>${data.cuer}</p>       
+                                            </div>
+                                        </div>
+                                            </div>
+                                        </div>`;
+                        cont = cont + 1;
+                        tbody.appendChild(div);
+                        cantP++;
+                    }
+                    aux++;
+                }while((aux!=response.posts.length) && (cantP<3))
+                
+            })
+    }else{mostrar0(0,3,1);}
+    
+    
+}
+
+function  cambioEstado(){
+    cant++;
+    fetch('/api/play', {
         method: "GET"
     }).then(res => res.json())
         .then(response => {
-            //var respo = [];   
-            // respo.add(response[i])
-            let parent = document.getElementById("sec1");
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
+           
+            if(response.recordatorio.length==cant){
+                cant=0;
+            }
+            let data = {
+                tit: (response.recordatorio[cant]).titulo,
+                img: (response.recordatorio[cant]).imagenExtension,
+                usr: (response.recordatorio[cant]).usuario,
+                galeria: (response.recordatorio[cant]).galeria
             };
-            parent.innerHTML = `<ul class="pagination" style="position: absolute; left: 25%; bottom: 0;">                           
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0(0,3,1)">1</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0(3,6,2)">2</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0(6,9,3)">3</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0(9,12,4)">4</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0(12,15,5)">5</a></li>
-                                </ul>`;
-            let cont = 1;
-            let aux=0;
-            let cantP=0;
-            do{
-                let idd = 'modal';
-                let hash = '#';
-                console.log(response.posts);
-                if(data1==(response.posts[aux]).titulo){
-                    let data = {
-                        tit: (response.posts[aux]).titulo,
-                        cuer: (response.posts[aux]).texto,
-                        aut: (response.posts[aux]).autor
-                    };
-                    let tbody = document.getElementById("sec1");
-                    let div = document.createElement("div");
-                    div.innerHTML = `<div class="sectionn">
-                                        <div class="">
-                                        <div class="card-panel" style = "background-color: #40445a">
-                                        <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
-                                            ${data.tit}</a>
-                                        <a class="linkss waves effect waves-light">${data.aut}</a>
-                                        <p>${data.cuer}</p>       
-                                        </div>
-                                    </div>
-                                        </div>
-                                    </div>`;
-                    cont = cont + 1;
-                    tbody.appendChild(div);
-                    cantP++;
-                }
-                aux++;
-            }while((aux!=response.posts.length) && (cantP<3))
+            let tbody = document.getElementById("div123");
+            tbody.removeChild(tbody.firstChild);
+            let div2 = document.createElement("div");
+            var pub ="";
+            for(let j = 0; j< data.galeria.length; j++){                      
+                var pub = "<tr>"+"<td>"+data.galeria[j].nombre+"</td>"+"</tr>"+pub;
+            }
+            div = `<div class="card" style = "background-color: #40445a">
+                                <div class="card-content cancion3" >
+                                    <img src = "images/${data.img}" style = "width: 100%" onClick="cambioEstado()">
+                                    <h5>${data.tit}</h5>
+                                    <p>${data.usr}</p>
+                                    <table>
+                                        <thead>
+                                            <th>Notas</th>
+                                        </thead>
+                                        <tbody>
+                                        `;
+            div +=pub+"</tbody>"+"</table>"+"</div>"+"</div>"
+            div2.innerHTML = div;
+            tbody.appendChild(div2);
             
         })
 }
